d129f36079ad4a9c02aeec60d2d7402fff2dcdf3
Joao Elias Engelbrecht Kayser <jeliasek@gmail.com>
2021-04-22 22:41:21 -0300

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+interface ButtonProps extends TouchableOpacityProps {
+    title: string,
+}
+export function Button( { title, ...rest }: ButtonProps ) {
+    return(
+        <TouchableOpacity
+            activeOpacity={0.7}
+            style={styles.container}
+            {...rest}
+        >
+            <Text style={styles.text}>
+                { title }
+            </Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: colors.green,
+        height: 56,
+        borderRadius: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+    },
+
+    text: {
+        fontSize: 16,
+        color: colors.white,
+        fontFamily: fonts.heading,
+    }
+})
